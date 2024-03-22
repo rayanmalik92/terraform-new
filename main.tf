de94e17a4c8ce82a76564c031408f4c01b99abb5
EC2 Default User <ec2-user@ip-172-31-89-19.ec2.internal>
2024-03-22 01:26:51 +0000

    removed security group attachment

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ resource "aws_instance" "Amazon-linux-instance-2" {
   ami              = "ami-0f403e3180720dd7e"
   instance_type    = "t2.medium"
   key_name         = "newkey"
-  vpc_security_group_ids = [aws_security_group.allow_8080.id]
   tags = {
     name = "New-jenkins-server"
   }
